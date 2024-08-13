6c3427d7d8145314ebe74a1a8445f6346110ae2b
Anshika Pandey <anshika.p2106@gmail.com>
2024-08-13 16:32:10 +0530

    passed 2 tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+let arr=string.slice("");
+let newarr=[];
+let j=0;
+for(let i=(arr.length)-1; i>=0;i--)
+{
+ newarr[j]=arr[i];
+ j++;
 
+}
+let newString= newarr.join("");
+return newString;
 };
 
 // Do not edit below this line
